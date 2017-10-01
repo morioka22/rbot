6ed4c09ab2139d93fcae9e1b453cc22748173453
takumi091111 <takumi091111@gmail.com>
2017-10-01 23:20:26 +0900

    Changed to return embed

--- lib/events/member.rb
+++ lib/events/member.rb
@@ -5,13 +5,34 @@ module Bot
             
             # メンバー参加時
             member_join do |event|
-                server = event.server
-                server.default_channel.send_message("#{event.user.mention}, Welcome to #{server.name}")
+                event.server.default_channel.send_embed do |embed|
+                    embed.title = "Member Joined"
+                    embed.color = 14103594
+                    embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(
+                        url: event.user.avatar_url
+                    )
+
+                    embed.add_field(
+                        name: event.user.name,
+                        value: "＿人人人人人人人人人人人人人人＿\n＞ㅤㅤゆっくりしていってね！ㅤ＜\n￣Ｙ^Ｙ^Ｙ^Ｙ^Ｙ^Ｙ^Ｙ^Ｙ^Ｙ^Ｙ￣"
+                    )
+                end
             end
 
             # メンバー退出時
             member_leave do |event|
-                server.default_channel.send_message("ByeBye...")
+                event.server.default_channel.send_embed do |embed|
+                    embed.title = "Member Left"
+                    embed.color = 14103594
+                    embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(
+                        url: event.user.avatar_url
+                    )
+
+                    embed.add_field(
+                        name: event.user.name,
+                        value: "Bye Bye..."
+                    )
+                end
             end
         end
     end
