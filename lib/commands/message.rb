dc1cb825541d26822c4ab9c116fade05a7a63ac9
takumi091111 <takumi091111@gmail.com>
2017-10-11 02:02:06 +0900

    Fix max amount of 'del' command

--- lib/commands/message.rb
+++ lib/commands/message.rb
@@ -14,8 +14,9 @@ module Bot
             end
 
             # メッセージ削除
-            command(:del, usage: 'del <件数>', description: 'メッセージの削除', min_args: 1, max_args: 99) do |event, n|
-                event.channel.prune(n.to_i + 1, true)
+            command(:del, usage: 'del <件数>', description: 'メッセージの削除', min_args: 1) do |event, n|
+                next if (n.to_i > 99)
+                event.channel.prune(n.to_i + 1, false)
                 nil
             end
         end
